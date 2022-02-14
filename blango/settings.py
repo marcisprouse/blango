1b0e78a8ebe354d464effa966d5d1404a7ab1c8b
Marci Sprouse <msmarcimichele@gmail.com>
2022-02-14 03:09:44 +0000

    Finish simplejwt

--- blango/settings.py
+++ blango/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 from configurations import Configuration
 from configurations import values
 import dj_database_url
+# related to our SIMPLE_JWTsettings
+from datetime import timedelta
 
 class Dev(Configuration):
 
@@ -47,6 +49,7 @@ class Dev(Configuration):
             "rest_framework.authentication.BasicAuthentication",
             "rest_framework.authentication.SessionAuthentication",
             "rest_framework.authentication.TokenAuthentication",
+            "rest_framework_simplejwt.authentication.JWTAuthentication"
         ],
         "DEFAULT_PERMISSION_CLASSES": [
             "rest_framework.permissions.IsAuthenticatedOrReadOnly"
@@ -72,6 +75,11 @@ class Dev(Configuration):
             "django_filters.rest_framework.DjangoFilterBackend",
             "rest_framework.filters.OrderingFilter"
         ],
+    }
+
+    SIMPLE_JWT = {
+        "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+        "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     }
 
     
--- blog/api/urls.py
+++ blog/api/urls.py
@@ -7,7 +7,8 @@ import os
 from rest_framework.routers import DefaultRouter
 # from blog.api.views import PostList, PostDetail, UserDetail, TagViewSet
 from blog.api.views import PostViewSet, UserDetail, TagViewSet
-
+# these are for SimpleJWT
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 
@@ -38,6 +39,9 @@ urlpatterns += format_suffix_patterns(urlpatterns)
 urlpatterns += [
     path("auth/", include("rest_framework.urls")),
     path("token-auth/", views.obtain_auth_token),
+    path("jwt/", TokenObtainPairView.as_view(), name="jwt_obtain_pair"),
+    path("jwt/refresh/", TokenRefreshView.as_view(), name="jwt_refresh"),
+
     re_path(r"^swagger(?P<format>\.json|\.yaml)$",
         schema_view.without_ui(cache_timeout=0),
         name="schema-json",
