e1140e5ae4138c3f73c7fb8a7307b02f33afc141
Marci Sprouse <msmarcimichele@gmail.com>
2022-02-14 02:14:25 +0000

    Finish pagination and django filter third party app

--- blango/settings.py
+++ blango/settings.py
@@ -63,6 +63,15 @@ class Dev(Configuration):
             "user_sustained": "5000/day",
             "user_burst": "100/minute",
         },
+
+        "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+
+        "PAGE_SIZE": 100,
+
+        "DEFAULT_FILTER_BACKENDS": [
+            "django_filters.rest_framework.DjangoFilterBackend",
+            "rest_framework.filters.OrderingFilter"
+        ],
     }
 
     
@@ -95,6 +104,7 @@ class Dev(Configuration):
     'rest_framework',
     'rest_framework.authtoken',
     'drf_yasg',
+    'django_filters'
     ]
 
     SITE_ID = 1
--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,9 @@ class Comment(models.Model):
 class Tag(models.Model):
     value = models.TextField(max_length=100, unique=True)
 
+    class Meta:
+        ordering = ["value"]
+
     def __str__(self):
         return self.value
 
@@ -31,6 +34,9 @@ class Post(models.Model):
     content = models.TextField()
     tags = models.ManyToManyField(Tag, related_name="posts")
     comments = GenericRelation(Comment)
+
+    class Meta:
+        ordering = ["created_at"]
 
     def __str__(self):
         return self.title
